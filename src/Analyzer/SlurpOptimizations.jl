b23d675c05508e7bd930c63c392f871b4945bd20
Fábio Cardeal da Natividade <ficarde@gmail.com>
2015-08-01 07:41:43 -0300

    Fix bug when a simple slurp has a constant binding
    `:(*{:C{x}},)` used to match any sequence of elements.

--- src/Analyzer/SlurpOptimizations.jl
+++ src/Analyzer/SlurpOptimizations.jl
@@ -9,22 +9,22 @@ export optimize_slurps!
 # dispatch function
 #-----------------------------------------------------------------------------------
 
-function optimize_slurps!(node)
+function optimize_slurps!(node, state)
   slurps = reverse(find(is_slurp, node.children))
-  optimize_slurps!(node, slurps, [])
+  optimize_slurps!(node, slurps, [], state)
 end
 
-function optimize_slurps!(node, slurps, after)
+function optimize_slurps!(node, slurps, after, state)
   isempty(slurps) && return
 
   i = slurps[1]
   slurp = node.children[i]
 
   if isempty(after) # last slurp
-    if is_simple_slurp(slurp)
+    if is_simple_slurp(slurp, state)
        node.children[i] = make_simple_last_slurp(slurp, node, i)
 
-    elseif is_simple_alternating_slurp(slurp)
+    elseif is_simple_alternating_slurp(slurp, state)
       # simple alternating last slurp | (..., *{a,b,c..})
     end
   else
@@ -34,17 +34,17 @@ function optimize_slurps!(node, slurps, after)
     if !isempty(equals)
       eqvalues = map(j->(node.children[i+j].check.value, i+j), equals)
 
-      if is_simple_slurp(slurp)
+      if is_simple_slurp(slurp, state)
         until, index = eqvalues[1]
         node.children[i] = make_simple_slurp_until_one(slurp, node, until, index-i)
         
-      elseif is_simple_alternating_slurp(slurp)
+      elseif is_simple_alternating_slurp(slurp, state)
 	# alternating slurp until X | (..., *{a,b,c...}, X, ...)
       end
     end
   end
   push!(after, i)
-  optimize_slurps!(node, slurps[2:end], after)
+  optimize_slurps!(node, slurps[2:end], after, state)
 end
 
 #-----------------------------------------------------------------------------------
@@ -67,14 +67,16 @@ make_simple_slurp_until_one(slurp, node, until, index) =
 # conditions
 #-----------------------------------------------------------------------------------
 
-function is_simple_slurp(slurp)
+function is_simple_slurp(slurp, state)
   length(slurp.children) == 1 &&
-  is_binding(slurp.children[1])
+  is_binding(slurp.children[1]) &&
+  !is_const_binding(slurp.children[1], state)
 end
 
-function is_simple_alternating_slurp(slurp)
+function is_simple_alternating_slurp(slurp, state)
   length(slurp.children) > 1 &&
-  all(is_binding, slurp.children)
+  all(is_binding, slurp.children) &&
+  !is_const_binding(slurp.children[1], state)
 end
 
 function is_binding(node::PatternGate)
@@ -82,5 +84,9 @@ function is_binding(node::PatternGate)
   isa(node.child, PatternLeaf)
 end
 is_binding(x) = false
+
+function is_const_binding(node::PatternGate, state)
+  node.check.name in state.consts
+end
 
 end
